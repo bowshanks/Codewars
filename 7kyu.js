7cee9658830c346dcf82b2d39ee7c294b0351a28
Daniel Gardner <bowshanks@gmail.com>
2017-01-12 10:35:40 -0700

    10th 7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -146,3 +146,53 @@ function tailSwap(arr) {
 function countRedBeads(n) {
   return n <= 0 ? 0:(n - 1) * 2
 }
+
+
+
+// String reverse slicing 101
+// Description:
+//
+// You'll be given a string of characters as an input.
+//
+// Create a function reverse_slice/reverseSlice that returns a list of strings: (a) in the reverse order of the original string, and (b) with each successive string starting one character further in from the end of the original string.
+// For example:
+//
+// '123'  becomes  ['321','21','1']
+// and
+//
+// 'abcde'  becomes  ['edcba','dcba','cba', 'ba', 'a']
+// Try to do this using slices and avoid converting the string to a list.
+function reverseSlice(str) {
+  var final = [];
+  for (var i = str.length;i > 0; i--) {
+    final.push(str.substring(0,i).split('').reverse().join(''));
+  }
+  return final;
+}
+
+//
+// All Star Code Challenge #24
+// Description:
+//
+// This Kata is intended as a small challenge for my students
+//
+// All Star Code Challenge #24
+//
+// Your friend David is an architect who is working on a triangle-focused design. He needs a quick way for knowing the measurement of a right triangle's side, only knowing two of the sides.
+//
+// He knows about the Pythagorean Theorem, but is too lazy to do the math. Help him by making 2 functions to ease his troubles.
+//
+// Create 2 functions:
+//
+// 1) hypotenuse(), which takes 2 integer arguments, the length of two regular sides of a right triangle, and returns the length of the missing side, the hypotenuse, as a number.
+//
+// 2) leg(), which takes 2 integer arguments, the first being the length of the hypotenuse, and the second being the length of a regular side of a right triangle. This function should return the length of the missing regular side, as a number.
+function hypotenuse(a, b){
+return Math.sqrt((a*a) + (b*b))
+
+}
+
+function leg(c, a){
+  return Math.sqrt((c*c) - (a*a))
+
+}
