55edf95304073624ed50b920265a8e35be6a586b
Daniel Gardner <bowshanks@gmail.com>
2017-01-17 13:25:15 -0700

    added more 6kyu

--- 6kyu.js
+++ 6kyu.js
@@ -47,3 +47,133 @@ function oneTwoThree(n) {
 //
 // 10.00 am => "840 minutes"
 // 23.59 pm => "1 minute"
+function minutesToMidnight(d){
+  var minutes = (24 * 60) - ((d.getHours() * 60) + d.getMinutes() + (d.getSeconds() > 30 ? 1:0))
+  return minutes + ' minute' + (minutes < 2 ? '':'s')
+}
+
+// Mexican capital 's phone number validator
+// Validate all numbers to make local calls from the capital city at the Mexican United States.
+//
+// The first two digits are the lada. Lada can only be 55 or 56 for those Mexico City phone numbers.
+//
+// The first two digits of the phone numbers when dialed locally are known as lada. To make phone numbers more readable some people write the lada between parenthesis. As you will see in further Katas lada concept is actually deeper than area code.
+//
+// Example
+//
+// Valid numbers:
+//
+// (56) 84 65 52
+// (56) 84 6552
+// (56) 846552
+// (56)846552
+// 56 84 65 52
+// 56 84 6552
+// 56 846552
+// 56846552
+// 55 95 64 85
+// 55 95 6485
+// 55 956485
+// 55956485
+// Non-Valid numbers:
+//
+// 99956485
+// abcdefgh
+// (56) 84 6 552
+
+//NEED TO REDO
+var stripNonNumeric = function(str) {
+  return str.split('').filter(function(currentValue){
+    return Number.isInteger(Number(currentValue.replace(' ','x')))
+  }).join('')
+}
+
+var isValidMXPhoneNumber = function(str) {
+  console.log(str);
+  str = stripNonNumeric(str);
+  return ((str.substring(0,2) == 55 || str.substring(0,2) == 56) && str.length == 8)
+};
+
+
+// Calculate String Rotation
+// Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second.
+//
+// For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+//
+// If the second string isn't a valid rotation of the first string, the method returns -1.
+// Examples:
+//
+// "coffee", "eecoff" => 2
+//
+// "eecoff", "coffee" => 4
+//
+// "moose", "Moose" => -1
+//
+// "isn't", "'tisn" => 2
+//
+// "Esham", "Esham" => 0
+//
+// "dog", "god" => -1
+function shiftedDiff(first,second){
+  first = first.split('');
+  second = second.split('');
+  var holder;
+  for (var i = 0; i < first.length; i++) {
+    if (first.join('') == second.join('')) {
+      break
+    }
+    else {
+      holder = first.pop();
+      first.unshift(holder);
+    }
+  }
+  return first.join('') !== second.join('') ? -1:i
+}
+
+// Basic Encryption
+// The most basic encryption method is to map a char to another char by a certain math rule. Because every char has an ASCII value, we can manipulate this value with a simple math expression. For example 'a' + 1 would give us 'b', because 'a' value is 97 and 'b' value is 98.
+//
+// You will need to write a method which does exactly that -
+//
+// get a string as text and an int as the rule of manipulation, and should return encrypted text. for example:
+//
+// encrypt("a",1) = "b"
+//
+// Full ascii table is used on our question (256 chars) - so 0-255 are the valid values.
+function cycle(start,n) {
+  while (n > 0) {
+    start++
+    if (start > 255) {
+      start = 0
+    }
+    n--
+  }
+  return start
+};
+
+function encrypt(text, rule) {
+  return text.split('').map(function(currentValue){
+    var x = cycle(currentValue.charCodeAt(0), rule);
+    return String.fromCharCode(x);
+  }).join('');
+};
+
+// String transformer
+// Given a string, return a new string that has transformed based on the input:
+//
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// For example:
+//
+// stringTransformer('Example Input')/string_transformer("Example Input") (depending on the language you are completing the Kata in) should return 'iNPUT eXAMPLE'
+//
+// You may assume the input only contain English alphabet and spaces.
+function stringTransformer(str) {
+  return str.split('').map(function(currentValue){
+    var x = currentValue.charCodeAt(0);
+    if (x == 32){
+      x = 0
+    }
+    return x > 90 ? String.fromCharCode(x - 32) : String.fromCharCode(x + 32);
+  }).join('').split(' ').reverse().join(' ')
+};
